7a37215e9772481b2c9530d2b886bed571cd994d
Lucas Pessolato <pessolatolucas@outlook.com>
2021-10-26 17:31:30 -0300

    update

---
+++ Exercicios/Filmes/Teste/senai_Filmes2_webAPI/senai_Filmes2_webAPI/Domains/UsuarioDomain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_Filmes2_webAPI.Domains
+{
+    public class UsuarioDomain
+    {
+        public int idUsuario { get; set; }
+
+        [Required(ErrorMessage = "Informe o email" )]
+        public string email { get; set; }
+
+        [Required(ErrorMessage ="informe a senha")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "O campo senha precisa ter no minimo 3 caracteres e no maximo 10")]
+        public string senha { get; set; }
+        public string permissao { get; set; }
+
+    }
+}
---
+++ Exercicios/Filmes/Teste/senai_Filmes2_webAPI/senai_Filmes2_webAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_Filmes2_webAPI.Interfaces
+{
+    interface IUsuarioRepository
+    {
+    }
+}
